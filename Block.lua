5ec34bb5bdd6d988dfbb0e69074b6914c83f5201
Yury Teplov <wanna.make.u.smile.yt@gmail.com>
2021-08-23 09:35:54 -0400

    Created Block class. It's the base class for all actors on map.

---
+++ Block.lua
@@ -0,0 +1,38 @@
+Block = {}
+
+function Block:new(posX, posY)
+    local public = {}
+    local private = {}
+
+    private.positionX = posX
+    private.positionY = posY
+
+    private.size = 30
+
+    function public:getPosition()
+        return private.positionX, private.positionY
+    end
+
+    function public:changePosition(dx, dy)
+        private.positionX = private.positionX + dx
+        private.positionY = private.positionY + dy
+    end
+
+    function public:setPosition(px, py)
+        private.positionX = px
+        private.positionY = py
+    end
+
+    function public:draw()
+        love.graphics.setColor(0, 0.4, 0.4)
+        love.graphics.rectangle("fill",
+                    private.positionX * private.size,
+                    private.positionY * private.size,
+                    private.size,
+                    private.size)
+    end
+
+
+    setmetatable(public, self)
+    self.__index = self; return public
+end
